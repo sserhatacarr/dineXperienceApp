631dc881172ab0cedad15f3e59d4ee61dcd388be
Serhat Acar <sserhatacarr@gmail.com>
2024-03-08 18:35:20 +0300

    feat(user-service): add RestaurantClient

---
+++ user-service/src/main/java/com/serhatacar/userservice/client/RestaurantClient.java
@@ -0,0 +1,25 @@
+package com.serhatacar.userservice.client;
+
+import com.serhatacar.restaurantservice.common.base.RestResponse;
+import com.serhatacar.restaurantservice.dto.RestaurantDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+/**
+ * @author Serhat Acar
+ */
+
+@FeignClient(name = "restaurant-service", url = "http://localhost:8080/api/v1")
+public interface RestaurantClient {
+
+    @GetMapping("/restaurant")
+    ResponseEntity<RestResponse<List<RestaurantDTO>>> getAllRestaurants();
+
+    @GetMapping ("/restaurant/{id)")
+    ResponseEntity<RestResponse<RestaurantDTO>> getRestaurantById(@PathVariable Long id);
+
+}

bbaadcce0187f064fac8e094b4107ae1df3f1d60
Serhat Acar <sserhatacarr@gmail.com>
2024-03-10 22:43:51 +0300

    test(user-service): add UserControllerIntegrationTest

---
+++ user-service/src/test/java/com/serhatacar/userservice/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,125 @@
+package com.serhatacar.userservice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.serhatacar.userservice.UserServiceApplication;
+import com.serhatacar.userservice.common.base.BaseControllerIntegrationTest;
+import com.serhatacar.userservice.dto.request.UserSaveRequest;
+import com.serhatacar.userservice.dto.request.UserUpdateRequest;
+import com.serhatacar.userservice.entity.enums.Gender;
+import com.serhatacar.userservice.entity.enums.Status;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Serhat Acar
+ */
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest(classes = {UserServiceApplication.class})
+ class UserControllerIntegrationTest extends BaseControllerIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+        objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    }
+
+    @Test
+    void shouldGetUsers() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/users"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        boolean success = isSuccess(mvcResult);
+        assertTrue(success);
+    }
+
+    @Test
+    void shouldGetUserById() throws Exception {
+        Long userId = 1L;
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/users/" + userId))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        boolean success = isSuccess(mvcResult);
+        assertTrue(success);
+    }
+
+    @Test
+    void shouldSaveUser() throws Exception {
+        UserSaveRequest userSaveRequest = new UserSaveRequest(
+                "name",
+                "surname",
+                LocalDate.now(),
+                "email@example.com",
+                Gender.MALE,
+                Status.ACTIVE,
+                40.7128,
+                74.0060
+        );
+
+        String requestAsString = objectMapper.writeValueAsString(userSaveRequest);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/users")
+                        .content(requestAsString)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        boolean success = isSuccess(mvcResult);
+        assertTrue(success);
+    }
+
+    @Test
+    void shouldUpdateUser() throws Exception {
+        UserUpdateRequest userUpdateRequest = new UserUpdateRequest(
+                1L,
+                "name",
+                "surname",
+                LocalDate.now(),
+                "email@example.com",
+                Gender.MALE,
+                Status.ACTIVE,
+                40.7128,
+                74.0060);
+
+        String requestAsString = objectMapper.writeValueAsString(userUpdateRequest);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/users/"+1L)
+                        .content(requestAsString)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    void shouldDeleteUser() throws Exception {
+        Long userId = 1L;
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/users/" + userId))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        boolean success = isSuccess(mvcResult);
+        assertTrue(success);
+    }
+}

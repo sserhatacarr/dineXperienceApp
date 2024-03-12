184511f5c8cddb5bbb16b73b86430e759beb746d
Serhat Acar <sserhatacarr@gmail.com>
2024-03-12 03:26:16 +0300

    fix(restaurant-service): add require fields

--- restaurant-service/src/main/java/com/serhatacar/restaurantservice/dto/response/RestaurantDTO.java
+++ restaurant-service/src/main/java/com/serhatacar/restaurantservice/dto/response/RestaurantDTO.java
@@ -8,17 +8,17 @@ import java.math.BigDecimal;
  * @author Serhat Acar
  */
 
-public record RestaurantDTO (
-                             Long id,
-                             String name,
-                             String address,
-                             String phone,
-                             String email,
-                             String description,
-                             String website,
-                             String workingHours,
-                             BigDecimal restaurantRate,
-                             Status status
-
-){
+public record RestaurantDTO(
+                                    Long id,
+                                    String name,
+                                    String address,
+                                    String phone,
+                                    String email,
+                                    String description,
+                                    String website,
+                                    String workingHours,
+                                    double latitude,
+                                    double longitude,
+                                    BigDecimal restaurantRate,
+                                    Status status) {
 }

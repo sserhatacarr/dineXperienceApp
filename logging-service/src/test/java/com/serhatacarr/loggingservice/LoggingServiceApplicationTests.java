33a99f612961411dad4e05c759de0ea7df29a4b1
Serhat Acar <sserhatacarr@gmail.com>
2024-03-14 21:28:28 +0300

    feat(logging-service): add ErrorLog entity

---
+++ logging-service/src/test/java/com/serhatacarr/loggingservice/LoggingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhatacarr.loggingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

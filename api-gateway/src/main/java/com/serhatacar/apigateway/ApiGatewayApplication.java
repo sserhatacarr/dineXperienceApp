5bcd604497118876da06fc45bcdd13f94b27e6e8
Serhat Acar <sserhatacarr@gmail.com>
2024-03-16 15:52:40 +0300

    chore(api-gateway):  add cors config

--- api-gateway/src/main/java/com/serhatacar/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/serhatacar/apigateway/ApiGatewayApplication.java
@@ -3,9 +3,13 @@ package com.serhatacar.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
---
+++ api-gateway/src/main/java/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Serhat Acar
+ */
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}

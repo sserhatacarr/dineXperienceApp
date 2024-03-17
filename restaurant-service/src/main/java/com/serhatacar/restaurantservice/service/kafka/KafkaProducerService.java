b7345896b596adb5947c5bd60ca13b18187e306b
Serhat Acar <sserhatacarr@gmail.com>
2024-03-17 15:12:53 +0300

    feat(user-service & restaurant-service): add KafkaProducerService

---
+++ restaurant-service/src/main/java/com/serhatacar/restaurantservice/service/kafka/KafkaProducerService.java
@@ -0,0 +1,22 @@
+package com.serhatacar.restaurantservice.service.kafka;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Serhat Acar
+ */
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducerService {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
+}

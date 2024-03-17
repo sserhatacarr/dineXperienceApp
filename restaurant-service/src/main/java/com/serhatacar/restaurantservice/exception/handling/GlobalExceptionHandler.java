5b8f100a485050a8a020e2a3dfddf31e87bcdae2
Serhat Acar <sserhatacarr@gmail.com>
2024-03-17 20:23:39 +0300

    feat(restaurant-service): add new handling exceptions

--- restaurant-service/src/main/java/com/serhatacar/restaurantservice/exception/handling/GlobalExceptionHandler.java
+++ restaurant-service/src/main/java/com/serhatacar/restaurantservice/exception/handling/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.serhatacar.restaurantservice.exception.handling;
 
 import com.serhatacar.restaurantservice.common.base.RestResponse;
 import com.serhatacar.restaurantservice.common.error.GeneralErrorMessageResponse;
+import com.serhatacar.restaurantservice.exception.notfound.ItemNotFoundException;
+import com.serhatacar.restaurantservice.exception.notfound.RecommendationNotFound;
+import com.serhatacar.restaurantservice.exception.notfound.RestaurantNotFoundException;
 import com.serhatacar.restaurantservice.service.kafka.KafkaProducerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +26,45 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<Object> handleException(Exception ex, WebRequest request) {
+        String message = ex.getMessage();
+        String description = request.getDescription(false);
+
+        var generalErrorMessages = new GeneralErrorMessageResponse(LocalDateTime.now(), message, description);
+        var restResponse = RestResponse.error(generalErrorMessages, message);
+
+        kafkaService.sendMessage("errorLog", "message = " + message + " description = " + description);
+
+        return ResponseEntity.badRequest().body(restResponse);
+    }
+
+    @ExceptionHandler(ItemNotFoundException.class)
+    protected ResponseEntity<Object> handleItemNotFoundException(ItemNotFoundException ex, WebRequest request) {
+        String message = ex.getMessage();
+        String description = request.getDescription(false);
+
+        var generalErrorMessages = new GeneralErrorMessageResponse(LocalDateTime.now(), message, description);
+        var restResponse = RestResponse.error(generalErrorMessages, message);
+
+        kafkaService.sendMessage("errorLog", "message = " + message + " description = " + description);
+
+        return ResponseEntity.badRequest().body(restResponse);
+    }
+
+    @ExceptionHandler(RestaurantNotFoundException.class)
+    protected ResponseEntity<Object> handleRestaurantNotFoundException(RestaurantNotFoundException ex, WebRequest request) {
+        String message = ex.getMessage();
+        String description = request.getDescription(false);
+
+        var generalErrorMessages = new GeneralErrorMessageResponse(LocalDateTime.now(), message, description);
+        var restResponse = RestResponse.error(generalErrorMessages, message);
+
+        kafkaService.sendMessage("errorLog", "message = " + message + " description = " + description);
+
+        return ResponseEntity.badRequest().body(restResponse);
+    }
+
+    @ExceptionHandler(RecommendationNotFound.class)
+    protected ResponseEntity<Object> handleRecommendationNotFound(RecommendationNotFound ex, WebRequest request) {
         String message = ex.getMessage();
         String description = request.getDescription(false);
 
